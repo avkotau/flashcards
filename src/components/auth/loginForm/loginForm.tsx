76c4e1c2cdee1d9a947616eea7a1f1115582071f
avkotau <kotov.alexander1@mail.ru>
2023-11-14 12:18:38 +0100

    feat: create emailRegex for a validate email inside input and validation for a password that must be more than 3 characters. Create logic for the error message, but don't show it yet.

--- src/components/auth/loginForm/loginForm.tsx
+++ src/components/auth/loginForm/loginForm.tsx
@@ -1,26 +1,55 @@
-import { useForm } from 'react-hook-form'
-
-import { CustomCheckbox } from '@/components/ui/checkbox'
+import { useController, useForm } from 'react-hook-form'
 
 import { Button } from '../../ui/button'
+import { CustomCheckbox } from '../../ui/checkbox'
 import { InputFactory } from '../../ui/input/input'
 
-type FormValues = {
+export type FormValues = {
   email: string
   password: string
   rememberMe: boolean
 }
 
 export const LoginForm = () => {
-  const { handleSubmit, register } = useForm<FormValues>()
+  const {
+    control,
+    formState: { errors },
+    handleSubmit,
+    register,
+  } = useForm<FormValues>()
 
   const onSubmit = (data: FormValues) => {}
 
+  const {
+    field: { onChange, value },
+  } = useController({
+    control,
+    defaultValue: false,
+    name: 'rememberMe',
+  })
+
+  const emailRegex =
+    /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/
+
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <InputFactory {...register('email')} label={'email'} />
-      <InputFactory {...register('password')} label={'password'} />
-      <CustomCheckbox {...register('rememberMe')} label={'remember me'} />
+      <InputFactory
+        {...register('email', {
+          pattern: { message: 'Invalid email', value: emailRegex },
+          required: 'Email is required',
+        })}
+        errorMessage={errors.email?.message}
+        label={'email'}
+      />
+      <InputFactory
+        {...register('password', {
+          minLength: { message: 'Password has to be at least 3 characters long', value: 3 },
+          required: 'Password is required',
+        })}
+        errorMessage={errors.email?.message}
+        label={'password'}
+      />
+      <CustomCheckbox checked={value} label={'remember me'} onCheckedChange={onChange} />
       <Button type={'submit'}>Submit</Button>
     </form>
   )
