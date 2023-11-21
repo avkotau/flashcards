8fffce32fa88a2397fae5780a55f83104034ccc5
avkotau <kotov.alexander1@mail.ru>
2023-11-21 19:56:18 +0100

    feat: Update SignInForm layout and styling. Enhanced SignInForm with Flexbox layout, specific margins for elements, DevTool integration, and styled 'Sign In' and 'Sign Up' buttons for improved UX.

--- src/components/auth/signInForm/signInForm.tsx
+++ src/components/auth/signInForm/signInForm.tsx
@@ -1,15 +1,17 @@
 import { JSX, forwardRef } from 'react'
 import { useForm } from 'react-hook-form'
 
+import { Button } from '@/components/ui/button'
 import { Card } from '@/components/ui/card'
-import { CustomCheckbox } from '@/components/ui/checkbox'
+import { ControlledCheckbox } from '@/components/ui/controlled/controlledCheckbox/controlledCheckbox'
 import { InputFactory } from '@/components/ui/input'
-import { signSchema } from '@/components/ui/signInForm/signSchema'
 import { Typography } from '@/components/ui/typography'
 import { DevTool } from '@hookform/devtools'
 import { z } from 'zod'
 
 import s from './signInForm.module.scss'
+
+import { signSchema } from './signSchema'
 
 type FormValues = z.infer<typeof signSchema>
 
@@ -21,9 +23,9 @@ export const SignInForm = forwardRef<HTMLFormElement, Props>(
 
     return (
       <Card className={s.wrapper}>
-        <Typography.Large>Sign In</Typography.Large>
-        <form onSubmit={handleSubmit(onSubmit)} ref={ref}>
-          <DevTool control={control} />
+        <Typography.Large className={s.title}>Sign In</Typography.Large>
+        <DevTool control={control} />
+        <form className={s.form} onSubmit={handleSubmit(onSubmit)} ref={ref}>
           <InputFactory
             label={'Email'}
             placeholder={'Enter email'}
@@ -37,8 +39,19 @@ export const SignInForm = forwardRef<HTMLFormElement, Props>(
             type={'password'}
             {...register('password')}
           />
-          <CustomCheckbox label={'Remember me'} />
+          <ControlledCheckbox
+            control={control}
+            defaultValue={false}
+            label={'remember me'}
+            name={'rememberMe'}
+          />
+          <Typography.Body2>Forgot Password?</Typography.Body2>
+          <Button fullWidth>Sign In</Button>
         </form>
+        <Typography.Body2>Don&apos;t have an account?</Typography.Body2>
+        <Button as={'a'} type={'submit'} variant={'link'}>
+          Sign Up
+        </Button>
       </Card>
     )
   }
