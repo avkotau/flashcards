2fb8fe218775a6522cbfc3595be827286bdc2a9f
avkotau <kotov.alexander1@mail.ru>
2023-12-05 14:12:09 +0100

    feat: Add sorting functionality to Table component. Implement sorting logic in 'getSortedData' function. Enable dynamic sorting in TableHeader using 'useSortHandler'. Apply CSS updates for sorting indicators and table layout.

---
+++ src/components/ui/table/TableHeader/tableHeader.tsx
@@ -0,0 +1,69 @@
+import { ComponentPropsWithoutRef, ElementRef, forwardRef } from 'react'
+
+import { ArrowDownIcon } from '@/assets/icons/components/arrowDownIcon'
+import { ArrowUpIcon } from '@/assets/icons/components/arrowUpIcon'
+import { Table } from '@/components/ui/table'
+import { Typography } from '@/components/ui/typography'
+
+import s from './tableHeader.module.scss'
+
+export type TableHeaderProps = {
+  isSorted?: boolean
+  key: string
+  title: string
+}
+
+export type Sort = {
+  direction: 'asc' | 'desc'
+  key: string
+} | null
+
+type Props = {
+  onSort?: (sort: Sort) => void
+  sort?: Sort
+  titleColumns: TableHeaderProps[]
+} & ComponentPropsWithoutRef<typeof Table.Head>
+
+const useSortHandler =
+  (sort: Sort | undefined, onSort: Props['onSort']) =>
+  (key: string, isSorted: boolean = false) => {
+    if (!onSort || !isSorted) {
+      return
+    }
+
+    const newSort: Sort =
+      sort?.key === key
+        ? { direction: sort.direction === 'asc' ? 'desc' : 'asc', key }
+        : { direction: 'asc', key }
+
+    onSort(newSort)
+  }
+
+export const TableHeader = forwardRef<ElementRef<typeof Table.Head>, Props>(
+  ({ key, onSort, sort, style, title, titleColumns, ...rest }, ref) => {
+    const handleSort = useSortHandler(sort, onSort)
+
+    return (
+      <Table.Head ref={ref} {...rest}>
+        <Table.Row>
+          {titleColumns.map(({ isSorted, key, title }) => {
+            return (
+              <Table.HeadCell key={key} onClick={() => handleSort(key, isSorted)}>
+                <Typography.Subtitle2>
+                  {title}
+                  {sort?.key === key &&
+                    key !== 'icons' &&
+                    (sort.direction === 'asc' ? (
+                      <ArrowUpIcon className={s.sortIcon} />
+                    ) : (
+                      <ArrowDownIcon className={s.sortIcon} />
+                    ))}
+                </Typography.Subtitle2>
+              </Table.HeadCell>
+            )
+          })}
+        </Table.Row>
+      </Table.Head>
+    )
+  }
+)
