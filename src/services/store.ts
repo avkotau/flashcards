807c3831e2ad84965b4cfd658ffd1a595f7b00db
avkotau <kotov.alexander1@mail.ru>
2023-12-06 21:52:51 +0100

    feat: create store, api, basic types from sever

---
+++ src/services/store.ts
@@ -0,0 +1,14 @@
+import { baseApi } from '@/services/base-api'
+import { configureStore } from '@reduxjs/toolkit'
+import { setupListeners } from '@reduxjs/toolkit/query/react'
+
+export const store = configureStore({
+  middleware: getDefaultMiddleware => getDefaultMiddleware().concat(baseApi.middleware),
+  reducer: {
+    [baseApi.reducerPath]: baseApi.reducer,
+  },
+})
+
+setupListeners(store.dispatch)
+export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>
