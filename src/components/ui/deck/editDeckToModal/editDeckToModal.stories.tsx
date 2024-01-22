9bf720725d71f3e744a85d476bdc393d5b92f16c
avkotau <kotov.alexander1@mail.ru>
2024-01-22 12:31:44 +0100

    fix: add decorators for EditDeckToModalCheck stories

--- src/components/ui/deck/editDeckToModal/editDeckToModal.stories.tsx
+++ src/components/ui/deck/editDeckToModal/editDeckToModal.stories.tsx
@@ -1,9 +1,19 @@
+import { Provider } from 'react-redux'
+
+import { store } from '@/app/store'
 import { EditIcon } from '@/assets'
 import { EditDeckToModal, IconButton } from '@/components'
 import { Meta, StoryObj } from '@storybook/react'
 
 const meta = {
   component: EditDeckToModal,
+  decorators: [
+    Story => (
+      <Provider store={store}>
+        <Story />
+      </Provider>
+    ),
+  ],
   tags: ['autodocs'],
   title: 'Components/EditDeckToModal',
 } satisfies Meta<typeof EditDeckToModal>
@@ -14,7 +24,7 @@ type Story = StoryObj<typeof meta>
 
 export const EditDeckToModalCheck: Story = {
   args: {
-    id: 'clm0rzxdu0csyvo2qnhi8j5bn',
+    id: 'clrnyxxwq006uy42wj18e3lks',
     isOpenModalBtn: <IconButton icon={<EditIcon />} />,
     title: 'Edit Deck',
     valueBtn: 'Save Changes',
