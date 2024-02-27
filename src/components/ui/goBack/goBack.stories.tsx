2e5dab7844b27078c786dca855b43b76730d3ece
avkotau <kotov.alexander1@mail.ru>
2024-02-27 19:41:31 +0100

    feat: logic for work goBack into a story change color arrow

--- src/components/ui/goBack/goBack.stories.tsx
+++ src/components/ui/goBack/goBack.stories.tsx
@@ -1,9 +1,18 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
+import { BrowserRouter } from 'react-router-dom'
+
 import { GoBack } from '@/components'
 
 const meta: Meta<typeof GoBack> = {
   component: GoBack,
+  decorators: [
+    Story => (
+      <BrowserRouter>
+        <Story />
+      </BrowserRouter>
+    ),
+  ],
   tags: ['autodocs'],
   title: 'Components/GoBack',
 }
