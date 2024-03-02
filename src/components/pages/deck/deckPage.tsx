dd5484f843d29128be8b6aa62d38de1599338ea8
avkotau <kotov.alexander1@mail.ru>
2024-03-02 19:59:44 +0100

    fix: add deckPage component, and router for it

---
+++ src/components/pages/deck/deckPage.tsx
@@ -0,0 +1,11 @@
+import { JSX } from 'react'
+
+import { GoBack } from '@/components'
+
+export const DeckPage = (): JSX.Element => {
+  return (
+    <>
+      <GoBack title={'Back to Decks List'} />
+    </>
+  )
+}
