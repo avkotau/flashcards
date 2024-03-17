fb8ab84f04d88acc9020c318578a1b9bc8ab1536
avkotau <kotov.alexander1@mail.ru>
2024-03-17 18:57:29 +0100

    fix: vercel

--- src/components/ui/addNewCard/addNewCard.stories.tsx
+++ src/components/ui/addNewCard/addNewCard.stories.tsx
@@ -1,5 +1,4 @@
 import { AddNewCard, Button } from '@/components'
-import { SelectItemType } from '@/components/ui/select/selectItem'
 import { Meta, StoryObj } from '@storybook/react'
 
 const meta = {
@@ -11,17 +10,8 @@ const meta = {
 export default meta
 type Story = StoryObj<typeof meta>
 
-const initialState: SelectItemType[] = [
-  { title: 'Select 1', value: '1' },
-  { title: 'Select 2', value: '2' },
-  { title: 'Select 3', value: '3' },
-  { title: 'Select 4', value: '4' },
-  { title: 'Select 5', value: '5' },
-]
-
 export const Default: Story = {
   args: {
-    initialState,
     isOpenModalBtn: <Button>Add New Card</Button>,
   },
 }
