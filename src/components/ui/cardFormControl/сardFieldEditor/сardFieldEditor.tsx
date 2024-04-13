b6a0eecbfb857774ed7785fe0b021156da7d5bfb
avkotau <kotov.alexander1@mail.ru>
2024-04-13 23:40:16 +0200

    feat: add onLoadImg logic to CardField

--- src/components/ui/cardFormControl/сardFieldEditor/сardFieldEditor.tsx
+++ src/components/ui/cardFormControl/сardFieldEditor/сardFieldEditor.tsx
@@ -1,6 +1,10 @@
+import { useState } from 'react'
 import { Control } from 'react-hook-form'
 
-import { CardFormType, ControlledInput, Typography } from '@/components'
+import { Button, CardFormType, ControlledInput, Typography, UploaderAvatar } from '@/components'
+import { ImageIcon } from '@radix-ui/react-icons'
+
+import s from '@/components/ui/deck/deckForm/deckForm.module.scss'
 
 type Props = {
   control: Control<CardFormType>
@@ -12,13 +16,24 @@ type Props = {
 }
 
 export const CardFieldEditor = ({ control, imageScr, label, name, selectFormat }: Props) => {
+  const [_, setCover] = useState<File | null>(null)
+
   const buttonUploadText = imageScr ? 'Change Cover' : ' Add Cover'
+  const onLoadImg = (data: File) => {
+    setCover(data)
+  }
 
   return (
     <>
       {selectFormat === 'text' && (
         <ControlledInput control={control} label={label} name={name} type={'text'} />
       )}
+      <UploaderAvatar className={s.uploader} onLoadAvatar={onLoadImg}>
+        <Button fullWidth type={'button'} variant={'secondary'}>
+          <ImageIcon />
+          <Typography.Subtitle2>{buttonUploadText}</Typography.Subtitle2>
+        </Button>
+      </UploaderAvatar>
       <Typography.Subtitle2>{buttonUploadText}</Typography.Subtitle2>
     </>
   )
